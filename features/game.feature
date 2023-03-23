8f1b1d7abf5554926e066b797b4ec5c4f3c5b07d
Freya Connor <freya.connor@softwire.com>
2023-03-23 09:39:41 +0000

    Deleted features/support/steps.js, features/support/world.js, features/game.feature, src/app.js, src/file_service.js, src/invalid_argument_exception.js, src/league.js, src/league_renderer.js, src/league_row.js, test/fixtures/.gitkeep, test/fixtures/in_progress_game.json, test/integration/game_test.js, test/app_test.js, test/league_renderer_test.js, test/league_test.js, .eslintrc.js, .gitignore, index.js, package-lock.json, package.json

--- features/game.feature
+++
@@ -1,6 +0,0 @@
-Feature: Run a table tennis league
-
-  Scenario: empty league
-    Given the league has no players
-    When I print the league
-    Then I should see that there are no players
--- src/invalid_argument_exception.js
+++
@@ -1,8 +0,0 @@
-class InvalidArgumentException extends Error {
-  constructor (message) {
-    super(message);
-    this.name = 'InvalidArgumentException';
-  }
-}
-
-module.exports = InvalidArgumentException;
